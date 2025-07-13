49eda762d584371a5131b347c08ad1f6e79bd3f6
merlin-morton <morton5@wisc.edu>
2025-07-13 03:49:01 -0500

    Hashrouter + Dummy Pages
    Added Hashrouter + Dummy Pages

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import { HashRouter, Route, Routes } from 'react-router'
+
+function About() {
+  return <h2>About Page</h2>;
+}
+
+export default About
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import { HashRouter, Route, Routes } from 'react-router'
+
+function Homepage() {
+  return <h2>Homepage</h2>;
+}
+
+export default Homepage
